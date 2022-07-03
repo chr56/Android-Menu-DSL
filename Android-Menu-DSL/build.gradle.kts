0bd047bed4352f941d8aef5b2b28b3b1e493544a
chr56 <chr0056@gmail.com>
2022-07-03 21:34:21 +0800

    [gradle] setup maven-publish

--- Android-Menu-DSL/build.gradle.kts
+++ Android-Menu-DSL/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -28,7 +29,30 @@ android {
     }
 }
 
+tasks.create("sourceJar", Jar::class.java) {
+    from(android.sourceSets.getByName("main").java.srcDirs)
+    archiveClassifier.set("sources")
+}
+
 dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.4.2")
+}
+
+val libVersion = "0.0.1"
+
+publishing {
+    publications {
+        this.create("release", MavenPublication::class.java) {
+
+            artifact(tasks.getByName("sourceJar"))
+            afterEvaluate {
+                artifact(tasks.getByName("bundleReleaseAar"))
+            }
+
+            groupId = "com.github.chr56"
+            artifactId = "android-menu-dsl"
+            version = libVersion
+        }
+    }
 }
