f6ae000e80b65443308e3e08d3cf40483e9b58e2
chr56 <chr0056@gmail.com>
2022-08-12 20:05:12 +0800

    [Util] add PropertyMapDelegate to store properties

---
+++ Android-Menu-DSL/src/main/java/com/github/chr56/android/menu_dsl/keys.kt
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2022 chr_56
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+@file:JvmName("Keys")
+
+package com.github.chr56.android.menu_dsl
+
+// Keys
+internal const val TITLE = 1
+
+internal const val ITEM_ID = 2
+internal const val GROUP_ID = 3
+
+internal const val ORDER = 4
+internal const val ICON = 5
+
+internal const val SHOW_AS_ACTION_FLAG = 6
+
+internal const val ENABLED = 7
+internal const val VISIBLE = 8
+internal const val CHECKABLE = 9
+internal const val CHECKED = 10
+
+internal const val MENU_ITEM_CLICK_LISTENER = 11
+
+internal const val CONTENT_DESCRIPTION = 12
+
+internal const val TOOLTIP_TEXT = 13
+
+internal const val TITLE_CONDENSED = 14
+
+internal const val INTENT = 15
+
+internal const val NUMERIC_SHORTCUT = 16
+internal const val ALPHABETIC_SHORTCUT = 17
+
+internal const val CUSTOM_ACTIONVIEW = 18
+internal const val ACTION_PROVIDER = 19
