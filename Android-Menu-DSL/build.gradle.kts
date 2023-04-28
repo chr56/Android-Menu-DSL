2b4fa96ae277bebe02192b5650fdf557cb0ee298
chr56 <chr0056@gmail.com>
2023-04-28 12:38:48 +0800

    [gradle] upgrade `androidx.annotation` to `1.5.0`

--- Android-Menu-DSL/build.gradle.kts
+++ Android-Menu-DSL/build.gradle.kts
@@ -48,7 +48,7 @@ tasks.create("sourceJar", Jar::class.java) {
 }
 
 dependencies {
-    implementation("androidx.annotation:annotation:1.3.0")
+    implementation("androidx.annotation:annotation:1.5.0")
     compileOnly("androidx.appcompat:appcompat-resources:1.3.1")
 }
 
