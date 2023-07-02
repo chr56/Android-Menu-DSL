3a4c3e31c73d5a8b963662529f700cc8aee28af1
chr56 <chr0056@gmail.com>
2023-07-02 11:45:32 +0800

    [gradle] upgrade `androidx.appcompat` to `1.5.1`

--- Android-Menu-DSL-TestApp/build.gradle.kts
+++ Android-Menu-DSL-TestApp/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation(project(":Android-Menu-DSL"))
 
     implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.6.1")
 }
