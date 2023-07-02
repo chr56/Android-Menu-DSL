e6d37212f90d4b5cacbbc5e22bf78aa8c1098b98
chr56 <chr0056@gmail.com>
2023-07-02 11:55:45 +0800

    [gradle] cleanup root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,8 @@
-buildscript {
-    val kotlinVersion by extra { "1.8.10" }
-    repositories {
-        mavenCentral()
-        google()
-        maven(url = "https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
-allprojects {
-    repositories {
-        mavenCentral()
-        google()
-        maven(url = "https://jitpack.io")
-    }
+plugins {
+    val agp = "8.0.2"
+    id("com.android.library") version agp apply false
+    id("com.android.application") version agp apply false
+    val kotlinVersion = "1.8.10"
+    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,20 @@
 rootProject.buildFileName = "build.gradle.kts"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
 include(":Android-Menu-DSL")
 include(":Android-Menu-DSL-TestApp")
