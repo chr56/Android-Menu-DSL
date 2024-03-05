05e46eb966b91c466405d0edd02e6d6874935a16
chr56 <chr0056@gmail.com>
2024-03-05 21:41:12 +0800

    [gradle] upgrade `androidx.core` to `1.12.0`

--- Android-Menu-DSL-TestApp/build.gradle.kts
+++ Android-Menu-DSL-TestApp/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 dependencies {
     implementation(project(":Android-Menu-DSL"))
 
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.9.0")
