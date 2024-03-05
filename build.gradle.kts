6ae36295c21d01efb2cff150c3d30b98cfb97a39
chr56 <chr0056@gmail.com>
2024-03-05 21:25:40 +0800

    [toolchains] upgrade kotlin to 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     val agp = "8.2.2"
     id("com.android.library") version agp apply false
     id("com.android.application") version agp apply false
-    val kotlinVersion = "1.8.10"
+    val kotlinVersion = "1.9.22"
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
 }
 
